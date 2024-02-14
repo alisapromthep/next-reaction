53b64e577ef53876f93cb57d95f608ceb34983d3
alisapromthep <alisa.promthep@gmail.com>
2024-02-14 14:09:35 -0800

    initial set up, register route

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+const RegisterPage = ()=>{
+
+    return(
+        <div>
+            <h1>Register here</h1>
+        </div>
+    )
+}
+
+export default RegisterPage;
