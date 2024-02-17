d2f1194f35a40cc390f746292a75e871b717ec04
alisapromthep <alisa.promthep@gmail.com>
2024-02-17 00:50:24 -0800

    set up handlechange in userContext

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,43 +1,50 @@
 import {createContext, useState} from 'react';
 
+
 interface UserInfoType {
-    name: string;
     username: string;
     password: string;
     email: string;
+    [key: string]: string;
 }
 
 interface UserContextType {
     userInfo: UserInfoType;
     isLogin: Boolean;
-    isRegister: Boolean;    
+    isRegister: Boolean;
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+export const UserContext = createContext<UserContextType | null>(null);
 
 const UserInfo:UserInfoType = {
-    name: "",
     username: "",
     password: "",
     email: "",
 }
-
-const UserContext = createContext<UserContextType>({
-    userInfo: UserInfo,
-    isLogin: false,
-    isRegister: false
-});
 
 export const UserProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
     const [userInfo, setUserInfo] = useState<UserInfoType>(UserInfo)
     const [isLogin, setIsLogin] = useState<Boolean>(false)
     const [isRegister, setIsRegister] = useState<Boolean>(false)
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target;
+        setUserInfo(prevUserInfo => ({
+            ...prevUserInfo,
+            [name]: value,
+        }));
+    };
+    
+
     //sign up user 
+
+
 
     
 
     return (
-        <UserContext.Provider value={{userInfo, isLogin, isRegister}}>
+        <UserContext.Provider value={{userInfo, isLogin, isRegister, handleChange}}>
             {children}
         </UserContext.Provider>
     )
