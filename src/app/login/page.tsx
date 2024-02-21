af668739a8b831e01487ac70a91c3c18937809b4
alisapromthep <alisa.promthep@gmail.com>
2024-02-20 23:07:44 -0800

    login page set up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,37 @@
 "use client"
 
 import React from 'react';
+import Input from '../../components/Forms/Input'; 
+import {useUserContext} from '../../context/userContext';
+
+
 const LoginPage = ()=>{
+
+    const {userInfo, handleChange, handleLogin} = useUserContext();
 
     return(
         <div>
-            <h1>Login here</h1>
+            <form onSubmit={handleLogin}>
+                <Input
+                labelName='username'
+                label='username'
+                inputName='username'
+                placeholder='username'
+                inputType='text'
+                onChange={handleChange}
+                />
+                <Input
+                labelName='password'
+                label='password'
+                inputName='password'
+                placeholder='password'
+                inputType='text'
+                onChange={handleChange}
+                />
+                <button type='submit'>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
