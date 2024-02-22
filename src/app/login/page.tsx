9ec0692c5ef69f686410298e19323bcbb4157484
alisapromthep <alisa.promthep@gmail.com>
2024-02-22 14:51:13 -0800

    login page styled

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import Input from '../../components/Forms/Input'; 
+import Image from 'next/image';
+import boyImg from '../../../public/images/watermelonboy.png';
 import {useUserContext} from '../../context/userContext';
 import Button from '../../components/Buttons/Button';
 
@@ -11,26 +13,36 @@ const LoginPage = ()=>{
     const {userInfo, handleChange, handleLogin} = useUserContext();
 
     return(
-        <div>
-            <form onSubmit={handleLogin}>
-                <Input
-                labelName='username'
-                label='username'
-                inputName='username'
-                placeholder='username'
-                inputType='text'
-                onChange={handleChange}
+        <div className="font-NunitoSans flex flex-col bg-green-light">
+            <div className="self-end pr-16">
+                <Image
+                src={boyImg}
+                width={250}
+                height={250}
+                alt="illustration of a guy holding a wage of watermelon"
                 />
-                <Input
-                labelName='password'
-                label='password'
-                inputName='password'
-                placeholder='password'
-                inputType='text'
-                onChange={handleChange}
-                />
-                <Button text="Submit" buttonType='submit'/>
-            </form>
+            </div>
+            <div className='rounded-t-3xl bg-white px-8'>
+                <form onSubmit={handleLogin}>
+                    <Input
+                    labelName='username'
+                    label='username'
+                    inputName='username'
+                    placeholder='username'
+                    inputType='text'
+                    onChange={handleChange}
+                    />
+                    <Input
+                    labelName='password'
+                    label='password'
+                    inputName='password'
+                    placeholder='password'
+                    inputType='text'
+                    onChange={handleChange}
+                    />
+                    <Button text="Login" buttonType='submit'/>
+                </form>
+            </div>
         </div>
     )
 }
