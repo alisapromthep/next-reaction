4982b223feaeb9fe4f73a8918e9839454f846f51
alisapromthep <alisa.promthep@gmail.com>
2024-02-22 14:06:05 -0800

    homepage styling, added all components, and links, partially styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from 'next/link';
 import heroImg from '../../public/images/home-hero.jpg';
+import {FaGithubSquare, FaLinkedin} from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className="bg-white flex flex-col font-NunitoSans">
+    <main className= "bg-white flex flex-col font-NunitoSans">
       <div className="bg-transparent">
         <Image
         src={heroImg}
@@ -13,13 +14,21 @@ export default function Home() {
         alt="illustration of three people with fruits"
         />
       </div>
-      <div className="flex flex-col items-center bg-green-light h-auto">
+      <div className="flex flex-col items-center bg-green-light rounded-t-3xl">
         <h1 className="text-green-dark font-bold text-3xl">Welcome to Track Reaction</h1>
-        <h2>Navigate life with allergies</h2>
+        <h2 className="font-bold text-green">Navigate life with allergies</h2>
         <p>Keep track and manage your allergies and food intolerance, and enjoy eating!</p>
-        <div>
-          <Link href='/register'>Register</Link>
-          <Link href='/login'>Login</Link>
+        <div className="py-4 rounded-xl border-1 border-white bg-white bg-opacity-50">
+          <Link className="p-4 rounded-xl" href='/register'>Register</Link>
+          <Link className="p-4 bg-white rounded-xl" href='/login'>Login</Link>
+        </div>
+        <div className="flex">
+          <a href='https://github.com/alisapromthep' target="_blank" rel="noreferrer">
+            <FaGithubSquare className="text-3xl"/>
+          </a>
+            <a href='https://www.linkedin.com/in/alisa-promthep/' target="_blank" rel="noreferrer">
+              <FaLinkedin className="text-3xl"/>
+            </a>
         </div>
         <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a> </p>
       </div>
