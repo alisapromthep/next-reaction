4a9275373d519aad62fb181adce81ebe23b77a5a
alisapromthep <alisa.promthep@gmail.com>
2024-02-22 16:06:33 -0800

    started on profile/dashboard page with newentryform, skeleton

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,13 @@
+import NewEntryForm from '../../../components/Forms/NewEntryForm';
+
+function profilePage() {
+
+    return (
+        <div>
+            <h1>Welcome Username</h1>
+            <NewEntryForm/>
+        </div>
+    )
+}
+
+export default profilePage;
