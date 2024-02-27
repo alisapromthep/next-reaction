24b85058f7c34afeea294c6f4a8dbd0f2142970a
alisapromthep <alisa.promthep@gmail.com>
2024-02-26 23:18:02 -0800

    added header component for header titles, added summary section into profile page

---
+++ src/components/Summary/SummarySection.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header/Header';
+
+
+function SummarySection(){
+    
+    return (
+        <article>
+            <Header headerText='Allergy log'/>
+        </article>
+    )
+}
+
+export default SummarySection;
