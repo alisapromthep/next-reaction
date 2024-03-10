8e492d4cdfe2f03a88f9c3304bf4ce6636bdf9ab
alisapromthep <alisa.promthep@gmail.com>
2024-03-09 23:40:33 -0800

    added function for todaysDate and timeNow for the newentry form

--- src/utility/dateAndTime.tsx
+++ src/utility/dateAndTime.tsx
@@ -1,10 +1,33 @@
 
 //convert date and time string from pocketbase 
-export const convertDate = (str) =>{
+export const convertDate = (str: string) => {
     return str.split(" ")[0]
 
 }
 
-export const convertTime = (str) =>{
+export const convertTime = (str: string) =>{
     return str.split(" ")[1]
+}
+
+export const getTodaysDate = () => {
+    const today = new Date();
+    const dd = today.getDate() < 10 ? `0${today.getDate()}`:`${today.getDate()}`
+    const mm = (today.getMonth()+1) < 10 ? `0${(today.getMonth()+1)}`:`${today.getMonth()+1}`
+    const yyyy = today.getFullYear();
+
+    const todayDate = `${yyyy}-${mm}-${dd}`;
+
+    return todayDate;
+
+}
+
+export const getTimeNow = ()=>{
+
+    const today = new Date();
+    const hours = today.getHours() < 10 ? `0${today.getHours()}`:today.getHours();
+    const minutes = today.getMinutes()<10 ? `0${today.getMinutes()}`:today.getMinutes();
+    const timeNow = `${hours}:${minutes}`
+
+    return timeNow;
+    
 }
