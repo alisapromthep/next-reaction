c7c1b424726c260296b64b5995f46fc487ca28c7
alisapromthep <alisa.promthep@gmail.com>
2024-03-11 22:58:53 -0700

    added handleSelecting symptom function for newEntryForm

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import pb from "../lib/pocketbase";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     console.log('running middleware')
     console.log(`middleware: ${request}`);
     const response = NextResponse.next();
@@ -11,6 +12,16 @@ export function middleware(request: NextRequest) {
         const userCookie = JSON.parse(requestCookie.value);
         const { token, username } = userCookie;
         const profileUrl = new URL(`/profile/${username}`, request.url);
+
+        try{
+            pb.authStore.isValid && (
+                await pb.collection("user").authRefresh()
+                //this ensures the user is actually logged in
+            ) 
+        } catch (err){
+            // clear auth store on failed refresh
+            pb.authStore.clear();
+        } 
         
         // Check if the current request is already targeting the profile route
         if (request.nextUrl.pathname !== profileUrl.pathname) {
