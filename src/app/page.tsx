1679a2fa3423a3be42c07fa6c4c5409cecc7342d
alisapromthep <alisa.promthep@gmail.com>
2024-03-14 01:06:06 -0700

    change in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
+"use client"
+
 import Image from "next/image";
 import Link from 'next/link';
 import heroImg from '../../public/images/home-hero.jpg';
 import {FaGithubSquare, FaLinkedin} from 'react-icons/fa';
 import NavBar from "@/components/NavBar/NavBar";
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
+import React, {FormEvent} from 'react';
+import {useAuthContext} from '@/context/authContext';
+
 
 export default function Home() {
+
+  const [login, setLogin] = useState<Boolean>(true);
+
   return (
-    <main className= "bg-white flex flex-col font-NunitoSans">
-      <div className="bg-transparent">
+    <main className= "h-screen bg-white flex font-NunitoSans">
+      <div className="w-full bg-transparent flex flex-col items-center justify-center">
         <Image
         src={heroImg}
         width={500}
         height={500}
         alt="illustration of three people with fruits"
         />
-      </div>
-      <div className="flex flex-col items-center bg-green-light rounded-t-3xl">
-        <h1 className="text-green-dark font-bold text-3xl">Welcome to Track Reaction</h1>
-        <h2 className="font-bold text-green">Navigate life with allergies</h2>
-        <p>Keep track and manage your allergies and food intolerance, and enjoy eating!</p>
-        <div className="py-4 rounded-xl border-1 border-white bg-white bg-opacity-50">
-          <Link className="p-4 rounded-xl" href='/register'>Register</Link>
-          <Link className="p-4 bg-white rounded-xl" href='/login'>Login</Link>
+        <div className="flex flex-col items-center">
+          <h1 className="text-green-dark font-bold text-3xl">Welcome to Track Reaction</h1>
+          <h2 className="font-bold text-green">Navigate life with allergies</h2>
+          <p>Keep track and manage your allergies and food intolerance, and enjoy eating!</p>
         </div>
         <div className="flex">
           <a href='https://github.com/alisapromthep' target="_blank" rel="noreferrer">
@@ -32,6 +38,13 @@ export default function Home() {
             </a>
         </div>
         <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a> </p>
+      </div>
+      <div className="w-full bg-green-light flex flex-col items-center rounded-l-3xl">
+        
+        <div className="py-4 rounded-xl border-1 border-white bg-white bg-opacity-50">
+          <button className="p-4 rounded-xl">Register</button>
+          <button className="p-4 bg-white rounded-xl">Login</button>
+        </div>
       </div>
     </main>
   );
