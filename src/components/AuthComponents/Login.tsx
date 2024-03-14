681fc3125ec9b44739c6897e248c7b178b83f052
alisapromthep <alisa.promthep@gmail.com>
2024-03-14 01:06:38 -0700

    created login and register as components

---
+++ src/components/AuthComponents/Login.tsx
@@ -0,0 +1,42 @@
+import Input from '../../components/Forms/Input'; 
+import Image from 'next/image';
+import {useAuthContext} from '../../context/authContext';
+import Button from '../../components/Buttons/Button';
+import { useRouter } from 'next/navigation';
+
+const Login = ()=>{
+
+    const router = useRouter();
+
+    const {currentUser, isLogin, handleChange, handleLogin} = useAuthContext();
+
+    if(isLogin){
+        router.push(`profile/${currentUser.username}`)
+    }
+
+    return(
+        <div className="font-NunitoSans flex flex-col bg-green-light">
+            <form onSubmit={handleLogin}>
+                <Input
+                labelName='username'
+                label='username'
+                inputName='username'
+                placeholder='username'
+                inputType='text'
+                onChange={handleChange}
+                />
+                <Input
+                labelName='password'
+                label='password'
+                inputName='password'
+                placeholder='password'
+                inputType='text'
+                onChange={handleChange}
+                />
+                <Button text="Login" buttonType='submit'/>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
