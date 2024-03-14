46504714eb22d9d8d911d8c582ca680f32b5ea3b
alisapromthep <alisa.promthep@gmail.com>
2024-03-14 02:10:14 -0700

    login component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,18 @@ import Link from 'next/link';
 import heroImg from '../../public/images/home-hero.jpg';
 import {FaGithubSquare, FaLinkedin} from 'react-icons/fa';
 import NavBar from "@/components/NavBar/NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 import React, {FormEvent} from 'react';
 import {useAuthContext} from '@/context/authContext';
+import Login from '@/components/AuthComponents/Login';
+import Register from "@/components/AuthComponents/Register";
 
 
 export default function Home() {
 
-  const [login, setLogin] = useState<Boolean>(true);
+  const [login, setLogin] = useState<boolean>(true);
+
 
   return (
     <main className= "h-screen bg-white flex font-NunitoSans">
@@ -39,11 +42,13 @@ export default function Home() {
         </div>
         <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a> </p>
       </div>
-      <div className="w-full bg-green-light flex flex-col items-center rounded-l-3xl">
-        
+      <div className="w-full bg-green-light flex flex-col items-center justify-center rounded-l-3xl">
+        {
+        login ? (<Login />):(<Register/>)
+        }
         <div className="py-4 rounded-xl border-1 border-white bg-white bg-opacity-50">
-          <button className="p-4 rounded-xl">Register</button>
-          <button className="p-4 bg-white rounded-xl">Login</button>
+          <button onClick={()=> setLogin(true)} className="p-4 rounded-xl">Register</button>
+          <button onClick={()=> setLogin(false)} className="p-4 bg-white rounded-xl">Login</button>
         </div>
       </div>
     </main>
