f2b2b1b93ee699a854656a4fd3d419afcd194941
alisapromthep <alisa.promthep@gmail.com>
2024-03-14 16:12:12 -0700

    mobile + tablet styling homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [login, setLogin] = useState<boolean>(true);
 
   return (
-    <main className= "h-screen bg-white flex font-NunitoSans">
+    <main className= "h-screen bg-white flex flex-col md:flex-row font-NunitoSans">
       <div className="w-full bg-transparent flex flex-col items-center justify-center">
         <Image
         src={heroImg}
@@ -26,33 +26,46 @@ export default function Home() {
         height={500}
         alt="illustration of three people with fruits"
         />
-        <div className="flex flex-col items-center">
-          <h1 className="text-green-dark font-bold text-3xl text-center">Welcome to Track Reaction</h1>
+        <div className="md:p-4 mb-2 flex flex-col items-center text-center">
+          <h1 className="text-green-dark font-bold text-xl md:text-3xl">Welcome to Track Reaction</h1>
           <h2 className="font-bold text-green">Navigate life with allergies</h2>
-          <p>Keep track and manage your allergies and food intolerance, and enjoy eating!</p>
+          <p className="text-sm md:text-base">Keep track and manage your allergies and food intolerance, and enjoy eating!</p>
         </div>
-        <div className="flex">
-          <a href='https://github.com/alisapromthep' target="_blank" rel="noreferrer">
-            <FaGithubSquare className="text-3xl"/>
-          </a>
-            <a href='https://www.linkedin.com/in/alisa-promthep/' target="_blank" rel="noreferrer">
-              <FaLinkedin className="text-3xl"/>
+        <div className="hidden md:block">
+          <div className="flex justify-center">
+            <a href='https://github.com/alisapromthep' target="_blank" rel="noreferrer">
+              <FaGithubSquare className="text-3xl"/>
             </a>
+              <a href='https://www.linkedin.com/in/alisa-promthep/' target="_blank" rel="noreferrer">
+                <FaLinkedin className="text-3xl"/>
+              </a>
+          </div>
+          <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a> </p>
         </div>
-        <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a> </p>
       </div>
-      <div className="w-full bg-green-light flex flex-col items-center justify-center rounded-l-3xl">
-        <div className="mt-5 h-3/6 flex items-center">
+      <div className="md:py-10 w-full bg-green-light flex flex-col items-center justify-center rounded-t-3xl md:rounded-r-none md:rounded-l-3xl">
+        <div className="mt-8 mb-6 md:my-0 h-3/6 flex items-center">
           {
           login ? (<Login />):(<Register/>)
           }
         </div>
-        <div className="group relative mt-5 rounded-xl border-1 border-white bg-white bg-opacity-50">
+        <div className="group relative my-6 md:mb-0 rounded-xl border-1 border-white bg-white bg-opacity-50">
           <div className={`w-3/6 h-full absolute top-0 bg-white rounded-xl fill-mode-forwards
           ${login ? "group-hover:animate-slide-left end-0":"group-hover:animate-slide-right left-0"}
           `}></div>
           <button onClick={()=> setLogin(false)} className="w-28 relative z-10 p-4 bg-transparent rounded-xl">Register</button>
           <button onClick={()=> setLogin(true)} className="w-28 relative z-10 p-4 bg-transparent rounded-xl">Login</button>
+        </div>
+        <div className="mt-6 mb-4 md:hidden">
+          <div className="flex justify-center">
+            <a href='https://github.com/alisapromthep' target="_blank" rel="noreferrer">
+              <FaGithubSquare className="text-3xl"/>
+            </a>
+              <a href='https://www.linkedin.com/in/alisa-promthep/' target="_blank" rel="noreferrer">
+                <FaLinkedin className="text-3xl"/>
+              </a>
+          </div>
+          <p>Made with ♡ by <a href="https://alisapromthep.dev/" target="_blank">Alisa</a></p>
         </div>
       </div>
     </main>
